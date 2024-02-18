3076290c77a1648a35251c0a1ab469be0dfba729
Jan Poonthong <janpoonthong628@gmail.com>
2024-02-18 19:21:37 +0700

    style: format code with prettier

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,50 +4,50 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/server/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
-  // callbacks: {
-  //     async session({ session, token, user }) {
-  //         // Send properties to the client, like an access_token and user id from a provider.
-  //         session.accessToken = token.accessToken;
-  //         session.user.id = token.id;
-  //         return session;
-  //     },
-  // },
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/",
-    signOut: "/",
-  },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    Credentials({
-      credentials: {
-        username: {},
-        password: {},
-      },
-      authorize: async (credentials) => {
-        console.log("Authorize")
-        if (credentials === undefined) return null;
-        const user = await prisma.user.findUnique({
-          where: {
-            name: credentials?.username,
-            password: credentials?.password,
-          },
-        });
+    // callbacks: {
+    //     async session({ session, token, user }) {
+    //         // Send properties to the client, like an access_token and user id from a provider.
+    //         session.accessToken = token.accessToken;
+    //         session.user.id = token.id;
+    //         return session;
+    //     },
+    // },
+    session: {
+        strategy: "jwt",
+    },
+    pages: {
+        signIn: "/",
+        signOut: "/",
+    },
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        Credentials({
+            credentials: {
+                username: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+                console.log("Authorize");
+                if (credentials === undefined) return null;
+                const user = await prisma.user.findUnique({
+                    where: {
+                        name: credentials?.username,
+                        password: credentials?.password,
+                    },
+                });
 
-        if (user) {
-          const mappedUser = {
-            id: user.id,
-            name: user.name,
-            role: user.role,
-          };
-          return mappedUser;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
+                if (user) {
+                    const mappedUser = {
+                        id: user.id,
+                        name: user.name,
+                        role: user.role,
+                    };
+                    return mappedUser;
+                } else {
+                    return null;
+                }
+            },
+        }),
+    ],
 };
 export const getServerAuthSession = () => getServerSession(authOptions);
--- src/ui/LoginPage.tsx
+++ src/ui/LoginPage.tsx
@@ -4,31 +4,31 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-  const router = useRouter();
-  async function handleLogin(formData: FormData) {
-    console.log("handleLogin")
-    try {
-      const response = await signIn("credentials", {
-        redirect: false,
-        username: formData.get("username") as string,
-        password: formData.get("password") as string,
-      });
-      console.log("response", response)
-      if (response?.error) throw new Error("Failed to login");
-      router.push("/dashboard");
-    } catch (error: any) {
-      console.log("Failed to login: ", error);
+    const router = useRouter();
+    async function handleLogin(formData: FormData) {
+        console.log("handleLogin");
+        try {
+            const response = await signIn("credentials", {
+                redirect: false,
+                username: formData.get("username") as string,
+                password: formData.get("password") as string,
+            });
+            console.log("response", response);
+            if (response?.error) throw new Error("Failed to login");
+            router.push("/dashboard");
+        } catch (error: any) {
+            console.log("Failed to login: ", error);
+        }
     }
-  };
 
-  return (
-    <main>
-      <h1>Login</h1>
-      <form action={handleLogin}>
-        <input name="username" type="text" placeholder="Username" />
-        <input name="password" type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Login</h1>
+            <form action={handleLogin}>
+                <input name="username" type="text" placeholder="Username" />
+                <input name="password" type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </main>
+    );
 }
