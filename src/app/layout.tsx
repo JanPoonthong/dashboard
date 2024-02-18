a38febaf43cc9946feeca172754fdd2c65d39847
Petchx <petchxpanuphong@gmail.com>
2024-02-18 19:38:32 +0700

    feat: add next ui, session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import { authOptions, getServerAuthSession } from "@/server/auth";
+import { getServerSession } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,14 +12,20 @@ export const metadata: Metadata = {
     description: "Assumption University TrueLab/D*CODE entrance website",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const session = await getServerAuthSession()
+    console.log("session", session);
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Providers session={session}>
+                    {children}
+                </Providers>
+            </body>
         </html>
     );
 }
---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+// app/providers.tsx
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react'
+import { SessionProvider } from 'next-auth/react'
+
+export function Providers({ children, session }: { children: React.ReactNode, session: any }) {
+    return (
+        <NextUIProvider>
+            <SessionProvider session={session}>
+                {children}
+            </SessionProvider>
+        </NextUIProvider>
+    )
+}
